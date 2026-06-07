bd4c0788d267b88e85b2dfd4edaa0ead5640853e
Pardhu <pardhasaradhireddy00@gmail.com>
2026-06-07 16:33:42 +0530

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -45,9 +48,23 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e,sys)
         
-if __name__=="__main__":
-    obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
 
-    data_transformation=DataTransformation()
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+if __name__ == "__main__":
+    print("started")
+
+    obj = DataIngestion()
+    print("object created")
+
+    train_data, test_data = obj.initiate_data_ingestion()
+    print("data ingestion done", train_data, test_data)
+
+    data_transformation = DataTransformation()
+    print("transformation object created")
+
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+    print("transformation done")
+
+    modeltrainer = ModelTrainer()
+    print("model trainer object created")
+
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
